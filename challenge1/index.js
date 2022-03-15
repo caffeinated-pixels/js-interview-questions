d378c354f770edfb7c7d449a150b6a91fe98f118
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-15 10:38:10 -0400

    implement every indexOf solution

--- challenge1/index.js
+++ challenge1/index.js
@@ -1,1 +1,31 @@
+/* 
+CHALLENGE BRIEF: 
+check if string is pangram (contains all letters of English alphabet)
+
+Gotchas:
+letter casing
+spaces
+*/
+
 console.log('dave')
+
+const abc = 'abcdefghijklmnopqrstuvwxyz'
+
+const isPangram1 = (string) => {
+  const abcArray = abc.split('')
+  const lcString = string.toLowerCase()
+
+  console.log(abcArray.every((letter) => lcString.indexOf(letter) != -1))
+}
+
+isPangram1('The quick Brown fox jumps over the lazy DOG')
+isPangram1('abcdefghijklmnopqrstuvwxyz')
+
+/* 
+1. convert string to lowercase
+2. Remove spaces?
+
+3. iterate through abc with .every() & .indexOf()
+
+
+*/
