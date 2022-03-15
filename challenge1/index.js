b7657106c19b01090589f16cb7d9b7b6fb6045f2
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-15 11:25:13 -0400

    Add 2nd solution using for loop, update notes

--- challenge1/index.js
+++ challenge1/index.js
@@ -7,25 +7,51 @@ letter casing
 spaces
 */
 
-console.log('dave')
-
 const abc = 'abcdefghijklmnopqrstuvwxyz'
 
-const isPangram1 = (string) => {
+const checkPangram1 = (string) => {
   const abcArray = abc.split('')
-  const lcString = string.toLowerCase()
+  const testString = string.toLowerCase()
 
-  console.log(abcArray.every((letter) => lcString.indexOf(letter) != -1))
+  console.log(abcArray.every((letter) => testString.indexOf(letter) != -1))
 }
 
-isPangram1('The quick Brown fox jumps over the lazy DOG')
-isPangram1('abcdefghijklmnopqrstuvwxyz')
+checkPangram1('The quick Brown fox jumps over the lazy DOG')
+checkPangram1('abcdefghijklmnopqrstuvwxyz')
+checkPangram1('my name is DAVE')
 
-/* 
+/* Solution 1
+1. convert abc from string to array using .split(')
+2. convert input string to lowercase to create testString
+3. iterate through abcArray with .every()
+    - test if each letter occurs in testString using .indexOf()
+*/
+
+const checkPangram2 = (string) => {
+  const testString = string.toLowerCase()
+  let isPangram = true
+
+  for (let i = 0; i < abc.length; i++) {
+    const currentLetter = abc[i]
+    const isCurrentLetterAbsent = testString.indexOf(currentLetter) === -1
+    if (isCurrentLetterAbsent) {
+      isPangram = false
+      break
+    }
+  }
+
+  console.log(isPangram)
+}
+
+checkPangram2('the quick Brown fox jumps over the lazy DOG')
+checkPangram2('abcdefghijklmnopqrstuvwxyz')
+checkPangram2('my name is DAVE')
+
+/* Solution 2
 1. convert string to lowercase
-2. Remove spaces?
-
-3. iterate through abc with .every() & .indexOf()
-
-
+2. set isPangram variable to true
+3. for loop through every letter in abc
+    - check if currentLetter is present in testString using .indexOf()
+    - if currentLetter not present, set isPangram to false and break the loop
+4. return the value of isPangram (either true or false)
 */
