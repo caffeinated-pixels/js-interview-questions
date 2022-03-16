e96e527d576b33099891b8dd12280026fdde6bc9
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-16 15:25:05 -0400

    Add 1st solution using set

--- challenge4/index.js
+++ challenge4/index.js
@@ -6,7 +6,18 @@ Gotchas:
 - letter case
 */
 
-const isIsogram = (string) => {}
+const isIsogram = (string) => {
+  const arrayFromString = string.toLowerCase().split('')
+  const dedupedArrayFromString = [...new Set(arrayFromString)]
+
+  return dedupedArrayFromString.length === string.length
+}
 
 console.log(isIsogram('ambidExtrously')) // true
 console.log(isIsogram('patteRN')) // false
+
+/* 
+1. convert string to array of lowercase letters
+2. dedupe array using set, then convert back to array
+3. check if string.length equal dedupedArrayFromString.length
+*/
