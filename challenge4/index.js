5685c6196051ca8aea0014c672b08bd8dfda5043
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-16 15:19:59 -0400

    Add challenge 4 files

---
+++ challenge4/index.js
@@ -0,0 +1,12 @@
+/* 
+CHALLENGE BRIEF: 
+Implement an isogram check function. An isogram is string in which all letter are only present once. 
+
+Gotchas:
+- letter case
+*/
+
+const isIsogram = (string) => {}
+
+console.log(isIsogram('ambidExtrously')) // true
+console.log(isIsogram('patteRN')) // false
