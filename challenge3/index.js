f736158473be93d06f5c3d230c73ac589a3db6b4
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-16 14:47:39 -0400

    Add challenge 3 files

---
+++ challenge3/index.js
@@ -0,0 +1,8 @@
+console.log('hello world!')
+
+const toRainLanguage = (number) => {}
+
+console.log(toRainLanguage(3))
+console.log(toRainLanguage(21))
+console.log(toRainLanguage(35))
+console.log(toRainLanguage(1))
