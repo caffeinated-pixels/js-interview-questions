0061a82e7ca1ac56868961ab89a0924977744831
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-17 14:54:14 -0400

    Add 2nd encode solution

--- challenge6/index.js
+++ challenge6/index.js
@@ -1,9 +1,18 @@
 /* 
-Implement run length encode and decode functions. Run-length encoding is
-        a form of data compressions where consecutive data elements are replaced
-        by one data value and a count.
+Implement run length encode and decode functions. Run-length encoding is 
+a form of data compressions where consecutive data elements are replaced
+by one data value and a count.
+
+The brief doesn't stipulate how you should encode single elements, 
+ie should it be: 2x2d4e2t1y1x1z or 2x2d4e2tyxz?
+
+A quick Google suggests that it should be 2x2d4e2t1y1x1z, which means that Scrimba's solution is defective.
 */
 
+/********** 
+SOLUTION 1 
+using match() and reduce()
+**********/
 const encode = (string) => {
   const groups = string.match(/(\w)\1*/g)
   return groups.reduce((acc, curr) => (acc += `${curr.length}${curr[0]}`), '')
@@ -47,3 +56,52 @@ console.log(encode('xxddeeeettyxz')) // 2x2d4e2t1y1x1z
 
 console.log(decode('2u3a4o')) // uuaaaoooo
 console.log(decode('1r4b1t5s3x2w')) // rbbbbtsssssxxxww
+
+/********** 
+SOLUTION 2
+based on Scrimba solution of just using replace with its replacerFunction param
+**********/
+const encode2 = (string) => {
+  return string.replace(/(\w)\1*/g, (match, p1) => `${match.length}${p1}`)
+}
+
+/* steps:
+1. use replace method and pass in a function for generating the replacements
+  - as before it uses a regex that captures the first letter, then matches a sequence of that letter
+  - in the function, you can access the match (match) and the captured letter (p1) through params
+  - the function returns the match length concatenated to the letter
+  - the function is invoked for each match
+2. iterate through array with reduce() to ouput group length and single letter
+*/
+
+const decode2 = (string) => {
+  const encodedGroups = string.match(/\d\w/g)
+
+  return encodedGroups.reduce((acc, curr) => {
+    let string = ''
+    const loopsToRun = Number(curr[0])
+
+    for (let i = 0; i < loopsToRun; i++) {
+      string += curr[1]
+    }
+
+    return (acc += string)
+  }, '')
+}
+
+/* steps:
+1. use match & regex to create array of encodedgroups
+  - \d\w matches a digit followed by a letter
+2. iterate through array with reduce to decode the encodedgroups, for each iteration:
+  - create an empty string
+  - use the digit to set the number of for loopsToRun
+  - each loop adds a letter to string
+  - add string to the accumulator when the loop finishes
+3. return the accumulated value
+*/
+
+console.log('solution2', encode2('wwwiiuuuu')) // 3w2i4u
+console.log('solution2', encode2('xxddeeeettyxz')) // 2x2d4e2t1y1x1z
+
+console.log('solution2', decode2('2u3a4o')) // uuaaaoooo
+console.log('solution2', decode2('1r4b1t5s3x2w')) // rbbbbtsssssxxxww
