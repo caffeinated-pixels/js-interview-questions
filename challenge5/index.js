cd499ff5f6f01db3a913f4d442e54e126b669d9f
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-17 13:00:03 -0400

    Add 2nd solution using ternary

--- challenge5/index.js
+++ challenge5/index.js
@@ -5,6 +5,10 @@ Leap year rules:
   - the year is also evenly divisible by 400. Then it is a leap year.
 */
 
+/********** 
+SOLUTION 1 
+using sequential if statements
+**********/
 const isLeapYear = (year) => {
   if (year % 4 !== 0) return false
   if (year % 100 === 0 && year % 400 !== 0) return false
@@ -20,3 +24,20 @@ console.log(isLeapYear('2400')) // true
 console.log(isLeapYear('2200')) // false
 console.log(isLeapYear('2300')) // false
 console.log(isLeapYear('2500')) // false
+
+/********** 
+SOLUTION 2
+based on Scrimba's ternary statement
+**********/
+const isLeapYear2 = (year) => {
+  return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0
+}
+
+console.log(isLeapYear2('2020')) // true
+console.log(isLeapYear2('2018')) // false
+console.log(isLeapYear2('1700')) // false
+console.log(isLeapYear2('1600')) // true
+console.log(isLeapYear2('2400')) // true
+console.log(isLeapYear2('2200')) // false
+console.log(isLeapYear2('2300')) // false
+console.log(isLeapYear2('2500')) // false
