158ce8915109637e51b9f619789c09b8ef760496
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-17 13:16:21 -0400

    Setup files for challenge6

---
+++ challenge6/index.js
@@ -0,0 +1,12 @@
+/* 
+Implement run length encode and decode functions. Run-length encoding is
+        a form of data compressions where consecutive data elements are replaced
+        by one data value and a count.
+*/
+
+const encode = (string) => {}
+
+const decode = (string) => {}
+
+console.log(encode('wwwiiuuuu')) // 3w2i4u
+console.log(decode('2u3a4o')) // uuaaaoooo
