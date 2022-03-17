047d31a8873f3b833156f6e03deb4b81674b85aa
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-17 13:36:42 -0400

    Implement encode solution

--- challenge6/index.js
+++ challenge6/index.js
@@ -4,7 +4,15 @@ Implement run length encode and decode functions. Run-length encoding is
         by one data value and a count.
 */
 
-const encode = (string) => {}
+const encode = (string) => {
+  const groups = string.match(/(\w)\1+/g)
+  return groups.reduce((acc, curr) => (acc += `${curr.length}${curr[0]}`), '')
+}
+
+/* steps:
+1. use match & regex to create array of grouped letters
+2. iterate through array with reduce to ouput group length and single letter
+*/
 
 const decode = (string) => {}
 
