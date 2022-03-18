65a7f8a0945eceb5ee2247a0e0c28a93de4df511
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-18 13:29:43 -0400

    Setup challenge 10 files

---
+++ challenge10/index.js
@@ -0,0 +1,13 @@
+/* 
+Implement a function that transposes a matrix array.
+*/
+
+const transposeMatrix = (array) => {}
+
+console.log(
+  transposeMatrix([
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ])
+) // [[1,1,1],[2,2,2],[3,3,3]]
