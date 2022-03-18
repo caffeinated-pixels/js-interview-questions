e80c5e201d2d4ad5ffa133f3f7f115160ef34149
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-18 11:08:42 -0400

    Add 1st solution using .includes()

--- challenge7/index.js
+++ challenge7/index.js
@@ -2,7 +2,17 @@
 Write a function to remove the duplicates from an array. The only array method you can use is array.reduce(). 
 */
 
-const removeDuplicateValues = (array) => {}
+const removeDuplicateValues = (array) => {
+  return array.reduce((acc, curr) => {
+    if (acc.includes(curr)) return acc
+    return acc.concat(curr)
+  }, [])
+}
+/* 
+- intial value is empty array
+- if the accumulator (acc) array includes the current value (curr), we return the accumulator
+- else we add (concat) curr to acc
+*/
 
 console.log(
   removeDuplicateValues(['one', 'two', 'one', 'three', 'three', 'two'])
