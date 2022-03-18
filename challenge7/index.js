0bcf8b37233c38012380d18020c87d04deac7fde
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-18 10:58:32 -0400

    Setup files for challenge7

---
+++ challenge7/index.js
@@ -0,0 +1,9 @@
+/* 
+Write a function to remove the duplicates from an array. The only array method you can use is array.reduce(). 
+*/
+
+const removeDuplicateValues = (array) => {}
+
+console.log(
+  removeDuplicateValues(['one', 'two', 'one', 'three', 'three', 'two'])
+) // ['one','two','three']
