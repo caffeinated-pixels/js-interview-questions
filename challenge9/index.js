c52b76f63ddf5a3cd5cd78384d2018764492c83f
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-18 12:48:20 -0400

    Setup files for challenge9

---
+++ challenge9/index.js
@@ -0,0 +1,7 @@
+/* 
+Write a function that flattens an array using a recursive array.reduce(). 
+*/
+
+const makeFlat = (array) => {}
+
+console.log(makeFlat(['one', ['two', 'three'], ['four', ['five']]])) // ['one','two','three','four','five']
