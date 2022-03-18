fe515ce198976c0e20047312cfed00ad67a450ea
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-18 13:14:15 -0400

    Add 2nd solution

--- challenge9/index.js
+++ challenge9/index.js
@@ -2,6 +2,10 @@
 Write a function that flattens an array using a recursive array.reduce(). 
 */
 
+/********** 
+SOLUTION 1 
+using typeof & if statement
+**********/
 const makeFlat = (array) => {
   return array.reduce((acc, curr) => {
     if (typeof curr === 'object') curr = makeFlat(curr)
@@ -17,3 +21,24 @@ const makeFlat = (array) => {
 */
 
 console.log(makeFlat(['one', ['two', 'three'], ['four', ['five']]])) // ['one','two','three','four','five']
+
+/********** 
+SOLUTION 2 
+based on Scrimba solution of isArray & ternary
+**********/
+const makeFlat2 = (array) => {
+  return array.reduce((acc, curr) => {
+    return Array.isArray(curr) ? [...acc, ...makeFlat(curr)] : [...acc, curr]
+  }, [])
+}
+
+/* 
+  - call reduce on array
+    - use Array.isArray(curr) to check current value
+      - if it is an array make a recursive call and add the result to the array
+      - else add curr to the array
+    -  return the flattened array
+
+*/
+
+console.log(makeFlat2(['one', ['two', 'three'], ['four', ['five']]]))
