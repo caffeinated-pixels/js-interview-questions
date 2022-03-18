baf3edec14f833a250e7e25b04f24f35224b548b
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-18 13:02:19 -0400

    Add 1st solution

--- challenge9/index.js
+++ challenge9/index.js
@@ -2,6 +2,18 @@
 Write a function that flattens an array using a recursive array.reduce(). 
 */
 
-const makeFlat = (array) => {}
+const makeFlat = (array) => {
+  return array.reduce((acc, curr) => {
+    if (typeof curr === 'object') curr = makeFlat(curr)
+    return acc.concat(curr)
+  }, [])
+}
+
+/* 
+  - call reduce on array
+    - if current value is object (i.e. array), make recurive call makeFlat(curr)
+    - then add curr to acc using concat
+
+*/
 
 console.log(makeFlat(['one', ['two', 'three'], ['four', ['five']]])) // ['one','two','three','four','five']
