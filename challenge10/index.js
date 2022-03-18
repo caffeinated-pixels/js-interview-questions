5c3a1c7ad1fc790422e90593ac9bfd10b90b70f3
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-18 14:45:12 -0400

    Add 1st solution

--- challenge10/index.js
+++ challenge10/index.js
@@ -2,7 +2,12 @@
 Implement a function that transposes a matrix array.
 */
 
-const transposeMatrix = (array) => {}
+const transposeMatrix = (array) => {
+  return array.reduce((acc, curr) => {
+    curr.forEach((value, i) => (acc[i] = acc[i].concat(value)))
+    return acc
+  }, Array(array.length).fill([]))
+}
 
 console.log(
   transposeMatrix([
@@ -11,3 +16,10 @@ console.log(
     [1, 2, 3],
   ])
 ) // [[1,1,1],[2,2,2],[3,3,3]]
+
+/* 
+- call reduce on array
+- set initial acc to array containing 1 empty array per element inside of input array
+- loop through curr; append value to appropriate array inside acc (based on current index in curr)
+- return acc
+*/
