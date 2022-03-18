4bd6e1e583f8f01c03bd7592b14c56ca67781ff2
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-18 15:16:07 -0400

    Add 2nd solution

--- challenge10/index.js
+++ challenge10/index.js
@@ -2,6 +2,10 @@
 Implement a function that transposes a matrix array.
 */
 
+/********** 
+SOLUTION 1 
+using a combination of reduce & forEach
+**********/
 const transposeMatrix = (array) => {
   return array.reduce((acc, curr) => {
     curr.forEach((value, i) => (acc[i] = acc[i].concat(value)))
@@ -22,4 +26,36 @@ console.log(
 - set initial acc to array containing 1 empty array per element inside of input array
 - loop through curr; append value to appropriate array inside acc (based on current index in curr)
 - return acc
+
+note: this solution assumes that all nested arrays inside the input are:
+- the same length
+- contain the same numbers in the same order
+*/
+
+/********** 
+SOLUTION 2
+based on Scrimba's solution of using a nested map function
+**********/
+const transposeMatrix2 = (array) => {
+  return array.map((_, i) => array.map((value) => value[i]))
+}
+
+console.log(
+  transposeMatrix2([
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ])
+) // [[1,1,1],[2,2,2],[3,3,3]]
+
+/* 
+- call map on array
+- inside map function, call map on array again
+- we then use the index from the outer map function to grab the right value from the inner map function
+- essentially, we're running nested loops to populate each nested array with the same number
+- return the resulting array
+
+note: as with the above solution, this assumes that all nested arrays inside the input are:
+- the same length
+- contain the same numbers in the same order
 */
